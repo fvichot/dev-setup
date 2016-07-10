0b62225cada607cedb132eb5dcb9353ad88cf551
Florian Vichot <florian.vichot@gmail.com>
2016-07-10 15:31:37 +0200

    First commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Make a failed command and unset variables stop this script
+# Also disable filename expansions and fail on subcommands error in a pipe
+set -euf -o pipefail
+
+ln -sf $PWD/user_bashrc ~/.user_bashrc
+if [[ ! -f ~/.bashrc || -z $(grep user_bashrc ~/.bashrc) ]]; then
+  echo -e "\nsource ~/.user_bashrc" >> ~/.bashrc
+  echo "Added sourcing to bashrc"
+else
+  echo "Sourcing already active"
+fi
