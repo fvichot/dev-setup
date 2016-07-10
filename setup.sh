0ac1544ecf9cc5b8ac89fc1ed94672cbe57bb4a3
Florian Vichot <florian.vichot@gmail.com>
2016-07-10 15:44:33 +0200

    Fixes for bash_profile weirdness on macs, better test for active sourcing

--- setup.sh
+++ setup.sh
@@ -4,10 +4,20 @@
 # Also disable filename expansions and fail on subcommands error in a pipe
 set -euf -o pipefail
 
+BASHRC='.bashrc'
+if [[ $(uname -s) == 'Darwin ' ]]; then
+  BASHRC='.bash_profile'
+fi
+
+echo "Linking user_bashrc..."
 ln -sf $PWD/user_bashrc ~/.user_bashrc
+
+echo "Linking inputrc..."
 ln -sf $PWD/inputrc ~/.inputrc
-if [[ ! -f ~/.bashrc || -z $(grep user_bashrc ~/.bashrc) ]]; then
-  echo -e "\nsource ~/.user_bashrc" >> ~/.bashrc
+
+echo "Sourcing user_bashrc in bashrc..."
+if [[ ! -f ~/${BASHRC} || -z $(grep 'source ~/.user_bashrc' ~/${BASHRC}) ]]; then
+  echo -e "\nsource ~/.user_bashrc" >> ~/${BASHRC}
   echo "Added sourcing to bashrc"
 else
   echo "Sourcing already active"
